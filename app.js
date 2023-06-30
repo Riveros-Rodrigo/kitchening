4aa11bce89831f23d2387ef905fd582bf7417b4e
Rodrigo Riveros <riverosdev@gmail.com>
2023-06-29 21:38:56 -0300

    subo proyecto kitchen

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()//express ejecutado
+const PORT = 3030;
+
+app.use(express.static('public')) //ruteo los recursos estaticos
+
+/* RUTAS */
+app.get('/',(req,res) =>res.sendFile(path.join(__dirname,'views','home.html')))
+
+
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
